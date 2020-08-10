8ae62a3433f5bae361cf1d8f5b1ea51b48b8eff0
abdulkk49 <kadirkhimani5@gmail.com>
2020-08-10 17:14:24 +0530

    iChnage iterations

--- cheklen.py
+++ cheklen.py
@@ -7,6 +7,7 @@ pwd = dirname(realpath("__file__"))
 for i in range(1,86):
     with h5py.File(os.path.join(pwd, 'Embeddings/batch' + str(i) + ".h5") , 'r') as dataall:
         f  = dataall['embed']
+        print(i, len(f))
         count += len(f)
 
 print("Count : ", str(count))
